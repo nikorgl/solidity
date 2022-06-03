fc760a31327f188e9b14a9a10d1982b3b8db711a
nikorgl <nikorgl@mail.ru>
2022-06-03 15:06:04 +0700

    Multifunctional platform: tasks for DAO contract functions

--- 7_platform/tasks/index.ts
+++ 7_platform/tasks/index.ts
@@ -2,6 +2,9 @@ import "./deploys";
 import "./staking_claim.ts";
 import "./staking_stake.ts";
 import "./staking_unstake.ts";
+import "./dao_addProposal";
+import "./dao_finish";
+import "./dao_vote";
 
 import { task } from "hardhat/config";
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
